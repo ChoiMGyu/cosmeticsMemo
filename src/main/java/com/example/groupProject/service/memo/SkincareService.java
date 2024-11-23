9dba00ca3ccaad902f915251ff3e58973650d1db
choi <nippert@naver.com>
2024-11-23 19:57:55 +0900

    feat: 메모를 삭제하는 기능 구성

--- src/main/java/com/example/groupProject/service/memo/SkincareService.java
+++ src/main/java/com/example/groupProject/service/memo/SkincareService.java
@@ -7,4 +7,6 @@ public interface SkincareService {
     public Long saveSkincareMemo(Skincare skincare);
 
     public Skincare findById(Long id);
+
+    public void deleteSkincareMemo(Skincare skincare);
 }
