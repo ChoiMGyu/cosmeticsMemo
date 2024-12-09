8b4232c9d412b86b72bc3cb2bbe8d77a5f760bc2
choi <nippert@naver.com>
2024-12-09 18:22:01 +0900

    test: 스킨케어 컨트롤러 단위 테스트 구성

---
+++ src/test/java/com/example/groupProject/annotation/WithMockCustomUser.java
@@ -0,0 +1,19 @@
+package com.example.groupProject.annotation;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+
+    String username() default "account";
+
+    String password() default "1234";
+
+}
+
+
+
