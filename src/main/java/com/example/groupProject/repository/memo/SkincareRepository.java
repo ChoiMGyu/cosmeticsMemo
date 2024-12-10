9d92771808547417ed41cc61d173da6446320e46
choi <nippert@naver.com>
2024-12-10 14:31:55 +0900

    feat: 정렬 기준으로 페이징하여 반환하는 기능 구성

--- src/main/java/com/example/groupProject/repository/memo/SkincareRepository.java
+++ src/main/java/com/example/groupProject/repository/memo/SkincareRepository.java
@@ -4,11 +4,12 @@ import com.example.groupProject.domain.memo.Skincare;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface SkincareRepository extends JpaRepository<Skincare, Long> {
+public interface SkincareRepository extends JpaRepository<Skincare, Long>, JpaSpecificationExecutor<Skincare> {
 
-    @Query("SELECT s FROM Skincare s WHERE s.master.id = :id ORDER BY s.start_date ASC")
-    Page<Skincare> findAllByIdPaging(@Param("id") Long id, Pageable pageable);
+//    @Query("SELECT s FROM Skincare s WHERE s.master.id = :id ORDER BY s.start_date ASC")
+//    Page<Skincare> findAllByIdPaging(@Param("id") Long id, Pageable pageable);
 }
---
+++ src/main/java/com/example/groupProject/repository/memo/SkincareSpecifications.java
@@ -0,0 +1,35 @@
+package com.example.groupProject.repository.memo;
+
+import com.example.groupProject.domain.memo.Skincare;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SkincareSpecifications {
+
+    public static Specification<Skincare> withUserId(Long id) {
+        return (root, query, criteriaBuilder) -> {
+            query.orderBy(criteriaBuilder.asc(root.get("start_date")));
+            return criteriaBuilder.equal(root.get("master").get("id"), id);
+        };
+    }
+
+    public static Specification<Skincare> sortBy(String sortBy) {
+        return (root, query, criteriaBuilder) -> {
+            if (sortBy != null) {
+                switch (sortBy) {
+                    case "end_date":
+                        query.orderBy(criteriaBuilder.asc(root.get("end_date")));
+                        break;
+                    case "area":
+                        query.orderBy(criteriaBuilder.asc(root.get("area")));
+                        break;
+                    case "moisture":
+                        query.orderBy(criteriaBuilder.asc(root.get("moisture")));
+                        break;
+                    default:
+                        query.orderBy(criteriaBuilder.asc(root.get("start_date")));
+                }
+            }
+            return null;
+        };
+    }
+}
