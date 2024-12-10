e946928101f084321c8569b5a0bdc83d27ced932
choi <nippert@naver.com>
2024-12-10 18:18:33 +0900

    test: Specification을 사용하여 정렬 조건을 기준으로 메모를 페이징하여 반환하는 레포지토리 통합 테스트 구성

--- src/test/java/com/example/groupProject/repository/SkincareRepositoryTest.java
+++ src/test/java/com/example/groupProject/repository/SkincareRepositoryTest.java
@@ -1,132 +1,104 @@
 package com.example.groupProject.repository;
 
 import com.example.groupProject.domain.memo.Skincare;
+import com.example.groupProject.domain.user.RoleType;
+import com.example.groupProject.domain.user.User;
 import com.example.groupProject.repository.memo.SkincareRepository;
+import com.example.groupProject.repository.memo.SkincareSpecifications;
+import com.example.groupProject.repository.user.UserRepositoryImpl;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(locations = "classpath:application-test.yml")
+@Transactional
 public class SkincareRepositoryTest {
 
-    @PersistenceContext
+    @Autowired
     private EntityManager em;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
 
     @Autowired
     private SkincareRepository skincareRepository;
 
-    @Test
-    @DisplayName("특정 스킨케어 메모를 ID로 삭제할 수 있다")
-    public void ID_메모_삭제() throws Exception {
+
+    @ParameterizedTest
+    @DisplayName("회원의 스킨케어 메모를 정렬 기준별로 찾아온다")
+    @CsvSource({
+            "start_date, 기초케어 화장품 2, 기초케어 화장품",
+            "end_date, 기초케어 화장품 2, 기초케어 화장품",
+            "area, 기초케어 화장품 2, 기초케어 화장품 1",
+            "moisture, 기초케어 화장품 2, 기초케어 화장품 1",
+    })
+    public void 정렬기준_스킨케어_메모찾기(String sortBy, String expectedFirst, String expectedSecond) throws Exception {
         //given
+        User testUser = User.createUser("account", "password", null, null, null, null, RoleType.ROLE_USER);
+        userRepository.save(testUser);
+
         Skincare skincare = Skincare.builder()
-                .start_date(LocalDate.now())
+                .start_date(LocalDate.now().plusDays(1))
                 .name("기초케어 화장품")
                 .description("세안 후 첫 단계에 사용하는 화장품입니다.")
+                .master(testUser)
                 .area("얼굴")
+                .moisture("촉촉함")
                 .build();
 
-        skincareRepository.save(skincare);
-
-        //when
-        skincareRepository.deleteById(skincare.getId());
-        em.flush();
-        em.clear();
-
-        //then
-        Optional<Skincare> findSkincare = skincareRepository.findById(skincare.getId());
-        assertTrue(findSkincare.isEmpty());
-    }
-
-    @Test
-    @DisplayName("스킨케어 메모를 저장할 수 있다")
-    public void 스킨케어_메모_저장() throws Exception {
-        //given
-        Skincare skincare = Skincare.builder()
-                .start_date(LocalDate.now())
-                .name("기초케어 화장품")
-                .description("세안 후 첫 단계에 사용하는 화장품입니다.")
-                .area("얼굴")
-                .build();
-
-        //when
-        skincareRepository.save(skincare);
-        em.flush();
-        em.clear();
-
-        //then
-        Optional<Skincare> findSkincare = skincareRepository.findById(skincare.getId());
-        assertTrue(findSkincare.isPresent());
-        assertThat(findSkincare.get()).isEqualTo(skincare);
-    }
-
-    @Test
-    @DisplayName("스킨케어 메모를 ID로 찾아올 수 있다")
-    public void 스킨케어_메모_ID_찾기() throws Exception {
-        //given
-        Skincare skincare = Skincare.builder()
-                .start_date(LocalDate.now())
-                .name("기초케어 화장품")
-                .description("세안 후 첫 단계에 사용하는 화장품입니다.")
-                .area("얼굴")
-                .build();
-
-        skincareRepository.save(skincare);
-        em.flush();
-        em.clear();
-
-        //when
-        Optional<Skincare> findSkincare = skincareRepository.findById(skincare.getId());
-
-        //then
-        assertTrue(findSkincare.isPresent());
-        assertThat(findSkincare.get()).isEqualTo(skincare);
-    }
-
-    @Test
-    @DisplayName("스킨케어 메모를 모두 찾아볼 수 있다")
-    public void 스킨케어_모두찾기() throws Exception {
-        //given
         Skincare skincare1 = Skincare.builder()
-                .start_date(LocalDate.now())
-                .name("기초케어 화장품")
+                .start_date(LocalDate.now().plusDays(2))
+                .master(testUser)
+                .name("기초케어 화장품 1")
                 .description("세안 후 첫 단계에 사용하는 화장품입니다.")
-                .area("얼굴")
+                .area("몸")
+                .moisture("유분기 적음")
                 .build();
 
         Skincare skincare2 = Skincare.builder()
                 .start_date(LocalDate.now())
-                .name("헤어케어 화장품")
-                .description("세안 시 첫 단계에 사용하는 화장품입니다.")
+                .master(testUser)
+                .name("기초케어 화장품 2")
+                .description("세안 후 두 번째 단계에 사용하는 화장품입니다.")
                 .area("머리")
+                .moisture("유분기 많음")
                 .build();
 
+        skincareRepository.save(skincare);
         skincareRepository.save(skincare1);
         skincareRepository.save(skincare2);
         em.flush();
         em.clear();
 
         //when
-        List<Skincare> allSkincare = skincareRepository.findAll();
+        Pageable pageable = PageRequest.of(0, 2);
+        Specification<Skincare> spec = Specification.where(SkincareSpecifications.withUserId(testUser.getId()))
+                .and(SkincareSpecifications.sortBy(sortBy));
+
+        Page<Skincare> skincarePage = skincareRepository.findAll(spec, pageable);
+        List<Skincare> skincarePageContent = skincarePage.getContent();
 
         //then
-        assertThat(allSkincare).isNotNull();
-        assertThat(allSkincare.size()).isEqualTo(2);
+        assertThat(skincarePage).isNotNull();
+        assertThat(skincarePageContent.get(0).getName()).isEqualTo(expectedFirst);
+        assertThat(skincarePageContent.get(1).getName()).isEqualTo(expectedSecond);
     }
 }
