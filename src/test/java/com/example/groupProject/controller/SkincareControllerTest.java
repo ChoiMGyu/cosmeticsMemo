a3c5ec915fef7190f452f444d6cea24a17ce53a6
choi <nippert@naver.com>
2024-12-11 12:57:31 +0900

    test: Specification을 사용하여 정렬 조건을 기준으로 메모를 페이징하여 반환하는 컨트롤러 단위 테스트 구성

--- src/test/java/com/example/groupProject/controller/SkincareControllerTest.java
+++ src/test/java/com/example/groupProject/controller/SkincareControllerTest.java
@@ -13,6 +13,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -26,10 +28,13 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -66,13 +71,14 @@ public class SkincareControllerTest {
         user = User.createUser("account_test", "password", null, null, null, null, null);
         userService.join(user);
 
-        skincare = Skincare.builder()
+        skincare = spy(Skincare.builder()
                 .start_date(LocalDate.now())
                 .name("기초케어 화장품")
                 .description("세안 후 첫 단계에 사용하는 화장품입니다.")
                 .master(user)
                 .area("얼굴")
-                .build();
+                .moisture("촉촉함")
+                .build());
 
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
@@ -99,34 +105,56 @@ public class SkincareControllerTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("스킨케어 메모를 페이징하여 반환한다")
     @WithMockCustomUser
-    public void 페이징_반환() throws Exception {
+    @CsvSource({
+            "start_date",
+            "end_date",
+            "area",
+            "moisture"
+    })
+    public void 페이징_반환(String sortBy) throws Exception {
         //given
         Skincare skincare1 = Skincare.builder()
                 .start_date(LocalDate.now().plusDays(1))
                 .name("기초케어 화장품 1")
                 .description("세안 후 첫 단계에 사용하는 화장품입니다.")
-                .area("얼굴")
+                .area("몸")
+                .moisture("유분기 적음")
                 .build();
 
         Skincare skincare2 = Skincare.builder()
                 .start_date(LocalDate.now().plusDays(2))
                 .name("기초케어 화장품 2")
                 .description("세안 후 두 번째 단계에 사용하는 화장품입니다.")
-                .area("얼굴")
+                .area("머리")
+                .moisture("유분기 많음")
                 .build();
 
         SkincareDto skincareDto = SkincareDto.from(skincare);
         SkincareDto skincareDto1 = SkincareDto.from(skincare1);
         SkincareDto skincareDto2 = SkincareDto.from(skincare2);
 
-        Page<SkincareDto> skincarePage = new PageImpl<>(List.of(skincareDto, skincareDto1));
+        List<SkincareDto> skincareDtoMemos = List.of(skincareDto, skincareDto1, skincareDto2);
+        Comparator<SkincareDto> comparator = switch (sortBy) {
+            case "start_date" -> Comparator.comparing(SkincareDto::getStart_date);
+            case "end_date" -> Comparator.comparing(SkincareDto::getEnd_date);
+            case "area" -> Comparator.comparing(SkincareDto::getArea);
+            case "moisture" -> Comparator.comparing(SkincareDto::getMoisture);
+            default -> throw new IllegalArgumentException("지원하지 않는 정렬 기준입니다.");
+        };
+
+        List<SkincareDto> sortedSkincareDtoMemos = skincareDtoMemos.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+
+        Page<SkincareDto> skincarePage = new PageImpl<>(sortedSkincareDtoMemos.subList(0,2));
+        System.out.println("null판단::" + sortBy);
 
         when(userService.findByAccount(Mockito.anyString()))
                 .thenReturn(List.of(user));
-        when(skincareService.findAllSkincareMemoStartDatePage(eq(user.getId()), eq(0), eq(1)))
+        when(skincareService.findAllSkincareMemoPagingByUserId(eq(user.getId()), eq(0), eq(2), eq(sortBy)))
                 .thenReturn(skincarePage);
 
         //when
@@ -134,7 +162,8 @@ public class SkincareControllerTest {
         //then
         mockMvc.perform(get("/api/memo/skincare")
                         .param("page", "0")
-                        .param("size", "1")
+                        .param("size", "2")
+                        .param("sortBy", sortBy)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
