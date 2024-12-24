97a79e3b76cc03a03ecbb3ed86df5a7d09b611e3
choi <nippert@naver.com>
2024-12-24 18:30:50 +0900

    test: 좋아요 수 증가, 감소 구성

--- src/test/java/com/example/groupProject/service_unit/LikesServiceTest.java
+++ src/test/java/com/example/groupProject/service_unit/LikesServiceTest.java
@@ -5,7 +5,9 @@ import com.example.groupProject.domain.board.Likes;
 import com.example.groupProject.domain.user.RoleType;
 import com.example.groupProject.domain.user.SkinType;
 import com.example.groupProject.domain.user.User;
+import com.example.groupProject.repository.board.BoardRepository;
 import com.example.groupProject.repository.board.LikesRepository;
+import com.example.groupProject.repository.user.UserRepositoryImpl;
 import com.example.groupProject.service.board.LikesServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -32,10 +35,14 @@ public class LikesServiceTest {
     @Mock
     private LikesRepository likesRepository;
 
+    @Mock
+    private BoardRepository boardRepository;
+
+    @Mock
+    private UserRepositoryImpl userRepository;
+
     @InjectMocks
     private LikesServiceImpl likesService;
-
-
 
     @Spy
     private User user;
@@ -59,6 +66,7 @@ public class LikesServiceTest {
                 .title("Test Board")
                 .content("Test Content")
                 .master(user)
+                .like(1)
                 .build();
     }
 
@@ -66,53 +74,38 @@ public class LikesServiceTest {
     @DisplayName("좋아요를 눌렀을 때 숫자가 증가한다")
     public void 좋아요수_증가() throws Exception {
         //given
-        Likes likes = Likes.builder()
-                .user(user)
-                .board(board)
-                .like(0)
-                .build();
+        when(userRepository.findByAccount(any(String.class))).thenReturn(List.of(user));
+        when(boardRepository.findById(1L)).thenReturn(Optional.of(board));
 
         //when
-        likesService.incrementLike(board.getId(), user);
+        likesService.incrementLike(1L, anyString());
 
         //then
-        assertThat(likes.getLike()).isEqualTo(1);
+        assertThat(board.getLike()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("좋아요를 다시 눌렀을 때 숫자가 감소한다")
     public void 좋아요수_감소() throws Exception {
         //given
-        Likes likes = Likes.builder()
-                .user(user)
-                .board(board)
-                .like(1)
-                .build();
+        when(userRepository.findByAccount(any(String.class))).thenReturn(List.of(user));
+        when(boardRepository.findById(1L)).thenReturn(Optional.of(board));
 
         //when
-        likesService.decrementLike(likes.getId())
+        likesService.decrementLike(1L, anyString());
 
         //then
-        assertThat(likes.getLike()).isEqualTo(0);
+        assertThat(board.getLike()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("좋아요를 누른 사용자가 저장된다")
     public void 좋아요_사용자_저장() throws Exception {
         //given
-        Likes likes = Likes.builder()
-                .user(user)
-                .board(board)
-                .like(0)
-                .build();
-
-        LikesDto likesDto = LikesDto.from(likes);
 
         //when
-        likesService.saveLikes(likesDto, user);
 
         //then
-        verify(likesRepository, times(1)).save(likes);
     }
 
 
@@ -120,19 +113,10 @@ public class LikesServiceTest {
     @DisplayName("좋아요를 누른 사람을 찾을 수 있다")
     public void 좋아요_주인() throws Exception {
         //given
-        Likes likes = Likes.builder()
-                .user(user)
-                .board(board)
-                .like(0)
-                .build();
-
-        when(likesRepository.findById(any(Long.class))).thenReturn(Optional.of(likes));
 
         //when
-        Likes findLike = likesRepository.findById(1L).orElseThrow();
 
         //then
-        assertThat(findLike.getUser()).isEqualTo(user);
     }
 
     @Test
