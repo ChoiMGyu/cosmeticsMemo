77bfcaa1cf983132f449fc8218ae7b288cca216f
choi <nippert@naver.com>
2024-12-30 11:44:28 +0900

    test: 좋아요 증감에 대한 테스트 구성

--- src/test/java/com/example/groupProject/service_unit/LikesServiceTest.java
+++ src/test/java/com/example/groupProject/service_unit/LikesServiceTest.java
@@ -20,6 +20,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
@@ -27,10 +29,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("tests")
@@ -44,6 +44,12 @@ public class LikesServiceTest {
 
     @Mock
     private UserRepositoryImpl userRepository;
+
+    @Mock
+    private RedisTemplate<String, String> redisUserTemplate;
+
+    @Mock
+    private SetOperations<String, String> setOperations;
 
     @InjectMocks
     private LikesServiceImpl likesService;
@@ -72,100 +78,97 @@ public class LikesServiceTest {
                 .master(user)
                 .like(1)
                 .build();
+
+        when(redisUserTemplate.opsForSet()).thenReturn(setOperations);
     }
 
     private static Stream<Arguments> provideIncrementLikeTestCase() {
         return Stream.of(
-                Arguments.of("사용자가 좋아요를 처음 눌렀을 때 게시글의 좋아요 수가 증가한다", false, 2),
-                Arguments.of("사용자가 좋아요를 이미 누른 경우에는 게시글의 좋아요 수는 변하지 않는다", true, 1)
+                Arguments.of("사용자가 좋아요를 처음 눌렀을 때(Redis - X, DB - X) 게시글의 좋아요 수가 증가한다", false, false, false),
+                Arguments.of("사용자가 좋아요를 이미 누른 경우(Redis - O, DB - X)에는 좋아요 수는 변하지 않는다", true, false, true),
+                Arguments.of("사용자가 좋아요를 이미 누른 경우(Redis - X, DB - O)에는 게시글의 좋아요 수는 변하지 않는다", false, true, true),
+                Arguments.of("사용자가 좋아요를 이미 누른 경우(Redis - O, DB - O)에는 게시글의 좋아요 수는 변하지 않는다", true, true, true)
         );
     }
 
     @ParameterizedTest(name = "{index} - {0}")
     @MethodSource("provideIncrementLikeTestCase")
     @DisplayName("게시글 좋아요 증가 테스트")
-    public void 게시글_좋아요_증가(String description, boolean isAlreadyLiked, int expectedLikes) throws Exception
-    {
+    public void 게시글_좋아요_증가(String description, boolean redisExist, boolean dbExist, boolean isExceptionThrow) throws Exception {
         //given
-        Likes existingLike = Likes.builder()
-                .user(user)
-                .board(board)
-                .build();
+        long findBoardId = 1L;
+        String redisKey = "board_users:" + findBoardId;
+        when(boardRepository.findById(findBoardId)).thenReturn(Optional.of(board));
+        when(userRepository.findByAccount(anyString())).thenReturn(List.of(user));
 
-        when(userRepository.findByAccount(user.getAccount())).thenReturn(List.of(user));
-        when(boardRepository.findById(1L)).thenReturn(Optional.of(board));
+        when(setOperations.isMember(eq(redisKey), anyString())).thenReturn(redisExist);
 
-        if (isAlreadyLiked) {
-            when(likesRepository.findByUserAndBoard(user, board)).thenReturn(Optional.of(existingLike));
-        } else {
-            when(likesRepository.findByUserAndBoard(user, board)).thenReturn(Optional.empty());
+        if (!redisExist) {
+            if (dbExist) {
+                when(likesRepository.findByUserAndBoard(any(User.class), any(Board.class)))
+                        .thenReturn(Optional.of(Likes.builder().user(user).board(board).build()));
+            } else {
+                when(likesRepository.findByUserAndBoard(any(User.class), any(Board.class))).thenReturn(Optional.empty());
+            }
         }
 
         //when
-        likesService.incrementLike(1L, user.getAccount());
-
         //then
-        assertThat(board.getLike()).isEqualTo(expectedLikes);
+        if (isExceptionThrow) {
+            assertThrows(IllegalArgumentException.class,
+                    () -> likesService.incrementLike(findBoardId, user.getAccount()));
+        } else {
+            likesService.incrementLike(findBoardId, user.getAccount());
+            verify(redisUserTemplate.opsForSet(), times(1)).add(eq(redisKey), eq(user.getAccount()));
+            verify(likesRepository, times(1)).save(any(Likes.class));
+        }
     }
 
     private static Stream<Arguments> provideDecrementLikeTestCase() {
         return Stream.of(
-                Arguments.of("사용자가 좋아요를 누른 경우에만 숫자가 감소한다", true, 0),
-                Arguments.of("사용자가 좋아요를 누르지 않았을 경우에는 숫자가 감소하지 않는다", false, 1)
+                Arguments.of("사용자가 좋아요를 누른 경우(Redis - O, DB - O)에만 숫자가 감소한다", true, true, false),
+                Arguments.of("사용자가 좋아요를 누른 경우(Redis - O, DB - X)에만 숫자가 감소한다", true, false, false),
+                Arguments.of("사용자가 좋아요를 누른 경우(Redis - X, DB - O)에만 숫자가 감소한다", false, true, false),
+                Arguments.of("사용자가 좋아요를 누르지 않았을 경우(Redis - X, DB - X)에는 숫자가 감소하지 않는다", false, false, true)
         );
     }
 
     @ParameterizedTest(name = "{index} - {0}")
     @MethodSource("provideDecrementLikeTestCase")
     @DisplayName("게시글 좋아요 감소 테스트")
-    public void 게시글_좋아요_감소(String description, boolean isAlreadyLiked, int expectedLikes) throws Exception {
-        // given
-        Likes existingLike = Likes.builder()
-                .user(user)
-                .board(board)
-                .build();
+    public void 게시글_좋아요_감소(String description, boolean redisExist, boolean dbExist, boolean isNotAlreadyLike) throws Exception {
+        //given
+        long findBoardId = 1L;
+        String redisKey = "board_users:" + findBoardId;
+        when(boardRepository.existsById(findBoardId)).thenReturn(true);
+        when(setOperations.isMember(eq(redisKey), anyString())).thenReturn(redisExist);
 
-        when(userRepository.findByAccount(user.getAccount())).thenReturn(List.of(user));
-        when(boardRepository.findById(1L)).thenReturn(Optional.of(board));
-
-        if (isAlreadyLiked) {
-            when(likesRepository.findByUserAndBoard(user, board)).thenReturn(Optional.of(existingLike));
-        } else {
-            when(likesRepository.findByUserAndBoard(user, board)).thenReturn(Optional.empty());
+        if(!redisExist) {
+            if(dbExist) {
+                when(likesRepository.findByAccountJoinFetch(anyString()))
+                        .thenReturn(Optional.of(Likes.builder().user(user).board(board).build()));
+            }
+            else {
+                when(likesRepository.findByAccountJoinFetch(anyString()))
+                        .thenReturn(Optional.empty());
+            }
         }
 
-        // when
-        likesService.decrementLike(1L, user.getAccount());
-
-        // then
-        assertThat(board.getLike()).isEqualTo(expectedLikes);
-    }
-
-
-    @Test
-    @DisplayName("좋아요를 누른 사용자가 저장된다")
-    public void 좋아요_사용자_저장() throws Exception {
-        //given
-
         //when
-
         //then
-    }
-
-
-    @Test
-    @DisplayName("좋아요를 누른 사람을 찾을 수 있다")
-    public void 좋아요_주인() throws Exception {
-        //given
-
-        //when
-
-        //then
+        if(isNotAlreadyLike) {
+            assertThrows(IllegalArgumentException.class,
+                    () -> likesService.decrementLike(findBoardId, user.getAccount()));
+        } else {
+            likesService.decrementLike(findBoardId, user.getAccount());
+            verify(redisUserTemplate.opsForSet(), times(1)).remove(eq(redisKey), eq(user.getAccount()));
+            verify(likesRepository, times(1)).delete(any(Likes.class));
+        }
     }
 
     @Test
-    @DisplayName("게시글의 총 좋아요 수를 확인할 수 있다")
-    public void 총_좋아요수() throws Exception {
+    @DisplayName("Redis에 저장된 게시글의 좋아요 수를 DB와 동기화한다")
+    public void Redis_DB_동기화() throws Exception {
         //given
 
         //when
@@ -174,14 +177,13 @@ public class LikesServiceTest {
     }
 
     @Test
-    @DisplayName("Redis에서 게시글에 대한 좋아요 수에 대한 데이터를 초기화한다")
-    public void 좋아요_데이터_초기화() throws Exception {
+    @DisplayName("게시글의 좋아요 수를 Redis 또는 DB에서 읽어온다")
+    public void Redis_DB_좋아요수읽기() throws Exception {
         //given
 
         //when
 
         //then
     }
-
 
 }
