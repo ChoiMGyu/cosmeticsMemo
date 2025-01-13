3d397d3b53dbdaaf0cd7f8dc14344fe13ad30680
choi <nippert@naver.com>
2025-01-13 22:12:31 +0900

    test: Stomp header에 access token을 포함하여 연결을 시도하는 테스트 구성

--- src/test/java/com/example/groupProject/controller/ChatControllerTest.java
+++ src/test/java/com/example/groupProject/controller/ChatControllerTest.java
@@ -1,11 +1,9 @@
 package com.example.groupProject.controller;
 
-import com.example.groupProject.controller.chat.ChatController;
+import com.example.groupProject.config.util.JWTUtil;
+import com.example.groupProject.config.websocket.UnauthorizedException;
 import com.example.groupProject.dto.chat.ChatMessageDto;
 import com.example.groupProject.dto.chat.MessageSubDto;
-import com.example.groupProject.service.chat.ChatService;
-import com.example.groupProject.service.chat.RedisPublisher;
-import com.example.groupProject.service.chat.RedisSubscriber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,8 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -23,6 +20,8 @@ import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -36,6 +35,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,6 +47,9 @@ public class ChatControllerTest {
     private static final String SOCKET_URL = "ws://127.0.0.1:8080/ws-stomp";
     private static StompSession session;
     private static CountDownLatch latch;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @BeforeEach
     public void beforeEach() throws ExecutionException, InterruptedException {
@@ -61,16 +64,17 @@ public class ChatControllerTest {
         WebSocketStompClient stompClient = new WebSocketStompClient(transport);
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
-//        // Header 설정
-//        WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
-//        StompHeaders stompHeaders = new StompHeaders();
-//        stompHeaders.add(HttpHeaders.AUTHORIZATION, getAccessToken());
+        String accessToken = jwtUtil.createJwt("access", "account", "ROLE_USER", 3600000L);
+
+        // Header 설정
+        WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
+        webSocketHttpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+        StompHeaders stompHeaders = new StompHeaders();
+        stompHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
 
         // 서버 연결
-//        return stompClient.connect(SOCKET_URL, webSocketHttpHeaders, stompHeaders,
-//                getTestStompFrameHandler()).get();
         StompSessionHandler sessionHandler = new MyStompSessionHandler();
-        return stompClient.connectAsync(SOCKET_URL, sessionHandler).get();
+        return stompClient.connectAsync(SOCKET_URL, webSocketHttpHeaders, stompHeaders, sessionHandler).get();
     }
 
     private class MyStompSessionHandler extends StompSessionHandlerAdapter {
@@ -91,9 +95,37 @@ public class ChatControllerTest {
     }
 
     @Test
-    @DisplayName("WebSocket 연결 테스트")
+    @DisplayName("유효한 Access Token을 가져야 WebSocket을 연결할 수 있다")
     public void 서버_연결() throws Exception {
         assertThat(session.isConnected()).isTrue();
+    }
+
+    @Test
+    @DisplayName("유효하지 않은(만료된) Access Token으로 WebSocket을 사용할 때는 오류가 발생한다")
+    public void 유효하지않은_AccessToken() throws Exception {
+        //given
+        String expiredAccessToken = jwtUtil.createJwt("access", "account", "ROLE_USER", -3600000L); // 만료된 토큰 생성
+
+        WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
+        webSocketHttpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + expiredAccessToken);
+        StompHeaders stompHeaders = new StompHeaders();
+        stompHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + expiredAccessToken);
+
+        WebSocketClient transport = new SockJsClient(
+                Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient()))
+        );
+        WebSocketStompClient stompClient = new WebSocketStompClient(transport);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        // when
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+
+        assertThatThrownBy(() -> stompClient.connectAsync(SOCKET_URL, webSocketHttpHeaders, stompHeaders, sessionHandler).get())
+                .isInstanceOf(ExecutionException.class)
+                .satisfies(e -> {
+                    Throwable cause = e.getCause();
+                    assertThat(cause).isInstanceOf(HttpClientErrorException.Unauthorized.class);
+                });
     }
 
     @Test
@@ -134,5 +166,6 @@ public class ChatControllerTest {
 
         //RedisSubscriber log가 출력되면 성공하는 테스트 -> 추후 assertThat으로 변경 필요
     }
+
 
 }
